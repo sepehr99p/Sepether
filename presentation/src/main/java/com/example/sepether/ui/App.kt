7564592b9c8fd9a14771a04fae219c3a06f9678f
sepehr99p <sepehr.pourekrami@gmail.com>
2023-08-13 18:00:22 +0330

    init test activity & app

---
+++ presentation/src/main/java/com/example/sepether/ui/App.kt
@@ -0,0 +1,10 @@
+package com.example.sepether.ui
+
+import androidx.multidex.MultiDexApplication
+
+class App : MultiDexApplication() {
+
+
+
+}
+
---
+++ presentation/src/main/java/com/example/sepether/ui/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.sepether.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+
+
+}
