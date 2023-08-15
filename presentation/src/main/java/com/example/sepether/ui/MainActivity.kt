35a4fa8e40effad4ef3e25865ea15ea996255385
sepehr99p <sepehr.pourekrami@gmail.com>
2023-08-15 19:15:09 +0330

    ui update : MainActivity

--- presentation/src/main/java/com/example/sepether/ui/MainActivity.kt
+++ presentation/src/main/java/com/example/sepether/ui/MainActivity.kt
@@ -5,16 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.domain.entities.Forecastday
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -41,9 +46,19 @@ class MainActivity : ComponentActivity() {
         }
 
         Column(modifier = Modifier.fillMaxWidth()) {
-            TextC(value = "temp : "+currentWeather.current?.temp_c.toString())
-            TextC(value = "feels like : "+currentWeather.current?.feelslike_c.toString())
-            TextC(value = "wind : " +currentWeather.current?.wind_kph.toString() + " km/h")
+            Text(
+                modifier = Modifier.padding(16.dp),
+                text = "Today",
+                color = Color.Black,
+                fontSize = 18.sp
+            )
+            SimpleText(value = "temp : " + currentWeather.current?.temp_c.toString())
+            SimpleText(value = "feels like : " + currentWeather.current?.feelslike_c.toString())
+            SimpleText(value = "wind : " + currentWeather.current?.wind_kph.toString() + " km/h")
+            Divider(
+                modifier = Modifier.height(12.dp),
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+            )
             ForecastScreen()
         }
     }
@@ -56,22 +71,41 @@ class MainActivity : ComponentActivity() {
             forecastWeather.forecast
         }
 
-        Row(modifier = Modifier.fillMaxWidth()) {
-            forecastWeather.forecast?.forecastday?.let {
-                it.forEach {
-                    TextC("sunset : "+it.astro.sunset)
+        Column(modifier = Modifier.fillMaxWidth()) {
+            forecastWeather.forecast?.forecastday?.let { list ->
+                list.forEach {
+                    Day(forecastday = it)
                 }
             }
         }
-
     }
 
     @Composable
-    private fun TextC(value : String) {
+    private fun Day(forecastday: Forecastday) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                text = forecastday.date,
+                color = Color.Black,
+                fontSize = 18.sp
+            )
+            Spacer(
+                modifier = Modifier.height(4.dp)
+            )
+            SimpleText(value = " sunset : ${forecastday.astro.sunset}")
+            SimpleText(value = " sunrise : ${forecastday.astro.sunrise}")
+            SimpleText(value = " average temp : ${forecastday.day.avgtemp_c}")
+        }
+    }
+
+    @Composable
+    private fun SimpleText(value: String) {
         Text(
-            modifier = Modifier
-                .padding(16.dp),
-            text = value
+            modifier = Modifier.padding(16.dp),
+            text = value,
+            color = Color.Gray
         )
     }
 
