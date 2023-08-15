7d5512d886474014cb3f4b9800bb7e56fc1a6bdc
sepehr99p <sepehr.pourekrami@gmail.com>
2023-08-15 12:23:47 +0330

    feat : di impl for data & network

---
+++ presentation/src/main/java/com/example/sepether/di/core/DataModule.kt
@@ -0,0 +1,28 @@
+package com.example.sepether.di.core
+
+import com.example.data.api.WeatherApi
+import com.example.data.repository.WeatherRepositoryImpl
+import com.example.domain.repositories.WeatherRepository
+import com.example.domain.usecases.CurrentWeatherUseCase
+import com.example.domain.usecases.ForecastWeatherUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+
+    @Provides
+    @Singleton
+    fun provideWeatherRepository(weatherApi: WeatherApi) : WeatherRepository = WeatherRepositoryImpl(weatherApi)
+
+    @Provides
+    fun provideCurrentWeaetherUseCase(weatherRepository: WeatherRepository) : CurrentWeatherUseCase = CurrentWeatherUseCase(weatherRepository)
+
+    @Provides
+    fun provideForecastWeatherUseCase(weatherRepository: WeatherRepository) : ForecastWeatherUseCase = ForecastWeatherUseCase(weatherRepository)
+
+}
---
+++ presentation/src/main/java/com/example/sepether/di/core/NetworkModule.kt
@@ -0,0 +1,67 @@
+package com.example.sepether.di.core
+
+import com.example.data.api.WeatherApi
+import com.example.data.common.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.Protocol
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+
+
+
+    @Provides
+    @Singleton
+    fun provideWeatherApiService(
+        retrofit: Retrofit
+    ): WeatherApi {
+        return retrofit.create(WeatherApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        converterFactory: GsonConverterFactory,
+        httpClient: OkHttpClient.Builder
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(converterFactory)
+            .client(httpClient.build())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient.Builder {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .protocols(listOf(Protocol.HTTP_1_1))
+            .retryOnConnectionFailure(true)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+}
