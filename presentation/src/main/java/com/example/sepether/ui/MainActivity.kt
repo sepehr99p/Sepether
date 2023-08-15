f7eefad39766f60acab175c92bf881fa7a3d3c67
sepehr99p <sepehr.pourekrami@gmail.com>
2023-08-15 14:20:36 +0330

    fix : compose dependencies + test main activity

--- presentation/src/main/java/com/example/sepether/ui/MainActivity.kt
+++ presentation/src/main/java/com/example/sepether/ui/MainActivity.kt
@@ -1,9 +1,49 @@
 package com.example.sepether.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Row(modifier = Modifier.padding(all = 8.dp)) {
+                    Text(
+                        text = "TEST",
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.bodyLarge,
+                        textAlign = TextAlign.Center,
+                        color = Color.Gray
+                    )
+                    Text(
+                        text = "TEST",
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.bodyLarge,
+                        textAlign = TextAlign.Center,
+                        color = Color.Gray
+                    )
+                    Text(
+                        text = "TEST",
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.bodyLarge,
+                        textAlign = TextAlign.Center,
+                        color = Color.Gray
+                    )
+                }
+            }
+        }
+    }
 
 
 }
