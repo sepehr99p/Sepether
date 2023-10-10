ca78c3841686207727c8678ccd9a669b4c5aa3a8
sepehr99p <sepehr.pourekrami@gmail.com>
2023-10-10 15:53:33 +0330

    exo player dependencies added

--- presentation/src/main/java/com/example/sepether/ui/music/MusicPlayerService.kt
+++ presentation/src/main/java/com/example/sepether/ui/music/MusicPlayerService.kt
@@ -1,7 +1,25 @@
 package com.example.sepether.ui.music
 
-class MusicPlayerService {
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
 
+
+class MusicPlayerService : Service() {
+
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+
+    private fun startNotification() {
+
+    }
 
 
 }
