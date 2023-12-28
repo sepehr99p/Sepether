163029b2d8838ae1a46eba5476c34cc6b8b081c6
Sep <sepehr.pourekrami@gmail.com>
2023-12-28 17:15:10 +0330

    mpchart fixed finaly :)

---
+++ presentation/src/main/java/com/example/sepether/ui/weather/components/graphs/TempChart.kt
@@ -0,0 +1,11 @@
+package com.example.sepether.ui.weather.components.graphs
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+import com.github.mikephil.charting.charts.LineChart
+
+
+@Composable
+fun LineGraph() {
+
+}
