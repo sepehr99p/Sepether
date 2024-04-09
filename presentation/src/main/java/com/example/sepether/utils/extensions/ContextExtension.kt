0e0807b1833bae8a54df538fb853cf5ea386e07f
Sep <sepehr.pourekrami@gmail.com>
2024-04-09 17:15:04 +0330

    utils package classes updated

--- presentation/src/main/java/com/example/sepether/utils/extensions/ContextExtension.kt
+++ presentation/src/main/java/com/example/sepether/utils/extensions/ContextExtension.kt
@@ -4,24 +4,19 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.net.Uri
-import android.os.Build
 import android.provider.OpenableColumns
 import java.io.File
 
 val Context.isNetworkConnected: Boolean
     get() {
         val manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-            manager.getNetworkCapabilities(manager.activeNetwork)?.let {
-                it.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
-                        it.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
-                        it.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) ||
-                        it.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) ||
-                        it.hasTransport(NetworkCapabilities.TRANSPORT_VPN)
-            } ?: false
-        else
-            @Suppress("DEPRECATION")
-            manager.activeNetworkInfo?.isConnectedOrConnecting == true
+        return manager.getNetworkCapabilities(manager.activeNetwork)?.let {
+            it.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                    it.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                    it.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) ||
+                    it.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) ||
+                    it.hasTransport(NetworkCapabilities.TRANSPORT_VPN)
+        } ?: false
     }
 
 fun Context.getFileName(uri: Uri): String? = when (uri.scheme) {
--- presentation/src/main/java/com/example/sepether/utils/extensions/Modifier.kt
+++ presentation/src/main/java/com/example/sepether/utils/extensions/Modifier.kt
@@ -2,17 +2,18 @@ package com.example.sepether.utils.extensions
 
 import androidx.compose.foundation.background
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Brush
 
-fun Modifier.airQualityBackground() : Modifier =
-    composed { background(
-        brush = Brush.horizontalGradient(
-            colors = listOf(
-                MaterialTheme.colorScheme.primary,
-                MaterialTheme.colorScheme.primaryContainer
+fun Modifier.airQualityBackground(): Modifier =
+    composed {
+        background(
+            brush = Brush.horizontalGradient(
+                colors = listOf(
+                    MaterialTheme.colorScheme.primary,
+                    MaterialTheme.colorScheme.primaryContainer
+                )
             )
         )
-    ) }
+    }
