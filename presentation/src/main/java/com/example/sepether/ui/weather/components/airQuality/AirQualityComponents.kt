e06f6f315e00cda3fe324b6681085eef37c94922
Sep <sepehr.pourekrami@gmail.com>
2024-04-09 14:38:09 +0330

    hourly airQuality impl

--- presentation/src/main/java/com/example/sepether/ui/weather/components/airQuality/AirQualityComponents.kt
+++ presentation/src/main/java/com/example/sepether/ui/weather/components/airQuality/AirQualityComponents.kt
@@ -23,6 +23,10 @@ import com.example.sepether.systemDesign.theme.dimen.corner_8
 import com.example.sepether.systemDesign.theme.dimen.padding_2
 import com.example.sepether.systemDesign.theme.dimen.padding_4
 import com.example.sepether.systemDesign.theme.dimen.padding_8
+import com.example.sepether.utils.Constants.TIME_PATTERN
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun AirQualityComponent(modifier: Modifier = Modifier, data: AirQualityEntity? = null) {
@@ -31,31 +35,55 @@ fun AirQualityComponent(modifier: Modifier = Modifier, data: AirQualityEntity? =
     ) {
         Text(
             modifier = Modifier.padding(vertical = padding_8),
-            text = stringResource(id = R.string.air_quality),
+            text = stringResource(id = R.string.hourly_air_quality),
             color = MaterialTheme.colorScheme.onPrimary
         )
         data?.let { airQuality ->
             LazyRow {
-                items(airQuality.hourly.pm10) {
-                    AirQualityListItemComponent(value = it.toString())
+                with(airQuality.hourly) {
+                    items(pm10.zip(time)) {
+                        AirQualityListItemComponent(quality = it.first.toString(), hour = it.second)
+                    }
                 }
+
             }
         }
     }
 }
 
 @Composable
-private fun AirQualityListItemComponent(modifier: Modifier = Modifier, value: String) {
-    Row(
+private fun AirQualityListItemComponent(
+    modifier: Modifier = Modifier,
+    quality: String,
+    hour: String
+) {
+    Column(
         modifier = modifier
             .padding(padding_4)
             .clip(shape = RoundedCornerShape(corner_8))
             .background(color = MaterialTheme.colorScheme.primaryContainer)
             .padding(padding_4),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(text = value, color = MaterialTheme.colorScheme.onPrimary)
-        Text(modifier = Modifier.padding(start = padding_2),text = "μg/m³", color = MaterialTheme.colorScheme.onPrimary, fontSize = 8.sp )
+
+        ) {
+
+        val time = LocalDateTime.parse(
+            hour,
+            DateTimeFormatter.ofPattern(TIME_PATTERN)
+        )
+        Text(
+            text = time.hour.toString() + ":" + time.minute, color = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.6f)
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = quality, color = MaterialTheme.colorScheme.onPrimary)
+            Text(
+                modifier = Modifier.padding(start = padding_2),
+                text = "μg/m³",
+                color = MaterialTheme.colorScheme.onPrimary,
+                fontSize = 8.sp
+            )
+        }
     }
 }
 
@@ -63,7 +91,7 @@ private fun AirQualityListItemComponent(modifier: Modifier = Modifier, value: St
 @Preview
 @Composable
 private fun AirQualityListItemComponentPreview() {
-    AirQualityListItemComponent(value = "test")
+    AirQualityListItemComponent(quality = "test", hour = "2024-04-11T06:00")
 }
 
 
