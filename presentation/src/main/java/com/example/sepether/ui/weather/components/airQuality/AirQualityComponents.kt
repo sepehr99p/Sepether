8c1f09e6cbf2d1db63b8c0f67aca548f226cab35
Sep <sepehr.pourekrami@gmail.com>
2024-04-09 14:18:43 +0330

    airQualityComponent ui updated

--- presentation/src/main/java/com/example/sepether/ui/weather/components/airQuality/AirQualityComponents.kt
+++ presentation/src/main/java/com/example/sepether/ui/weather/components/airQuality/AirQualityComponents.kt
@@ -2,6 +2,7 @@ package com.example.sepether.ui.weather.components.airQuality
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -9,13 +10,17 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.domain.entities.AirQualityEntity
 import com.example.sepether.R
 import com.example.sepether.systemDesign.theme.dimen.corner_4
+import com.example.sepether.systemDesign.theme.dimen.corner_8
+import com.example.sepether.systemDesign.theme.dimen.padding_2
 import com.example.sepether.systemDesign.theme.dimen.padding_4
 import com.example.sepether.systemDesign.theme.dimen.padding_8
 
@@ -41,14 +46,16 @@ fun AirQualityComponent(modifier: Modifier = Modifier, data: AirQualityEntity? =
 
 @Composable
 private fun AirQualityListItemComponent(modifier: Modifier = Modifier, value: String) {
-    Column(
+    Row(
         modifier = modifier
             .padding(padding_4)
-            .clip(shape = RoundedCornerShape(corner_4))
+            .clip(shape = RoundedCornerShape(corner_8))
             .background(color = MaterialTheme.colorScheme.primaryContainer)
-            .padding(padding_4)
+            .padding(padding_4),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(text = value, color = MaterialTheme.colorScheme.onPrimary)
+        Text(modifier = Modifier.padding(start = padding_2),text = "μg/m³", color = MaterialTheme.colorScheme.onPrimary, fontSize = 8.sp )
     }
 }
 
